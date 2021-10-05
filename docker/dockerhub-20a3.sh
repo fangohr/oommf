b99e5f0f126a922ab693cfdcd3581655f1b848b8
Hans Fangohr <fangohr@soton.ac.uk>
2021-10-05 13:52:08 +0200

    Improve upload of new image to dockerhub

--- docker/dockerhub-20a3.sh
+++ docker/dockerhub-20a3.sh
@@ -10,3 +10,12 @@ docker build --no-cache -t oommf/oommf:20a3 .
 
 # push image to dockerhub
 echo "Run 'docker push oommf/oommf:20a3' to push to dockerhub"
+
+# the 'latest' tag is not added by DockerHub as one may guess:
+# https://medium.com/@mccode/the-misunderstood-docker-tag-latest-af3babfd6375
+# so if we want to allow our users to use 'docker pull oommf//oommf', then we need to define
+# the 'latest' tag manually.
+#
+echo "Also run these commands to set the 'latest' tag to the new image:"
+echo "  docker tag oommf/oommf:20a3 oommf/oommf:latest"
+echo "  docker push oommf/oommf:latest"
