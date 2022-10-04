8885ade3cdccd8561b096f98bbfa19030ec50439
Hans Fangohr <hans.fangohr@mpsd.mpg.de>
2022-10-04 15:03:07 +0200

    Updated container image on dockerhub with 2.0beta
    see https://hub.docker.com/r/oommf/oommf/tags

--- docker/push-image-to-dockerhub.sh
+++ docker/push-image-to-dockerhub.sh
@@ -6,10 +6,10 @@ docker login
 # build image (this will clone github.com/fangohr/oommf master)
 # - must make sure that is the right version, or update the
 # Dockerfile
-docker build --no-cache -t oommf/oommf:20a3 .
+docker build --no-cache -t oommf/oommf:20b0 .
 
 # push image to dockerhub
-echo "Run 'docker push oommf/oommf:20a3' to push to dockerhub"
+echo "Run 'docker push oommf/oommf:20b0' to push to dockerhub"
 
 # the 'latest' tag is not added by DockerHub as one may guess:
 # https://medium.com/@mccode/the-misunderstood-docker-tag-latest-af3babfd6375
@@ -17,5 +17,5 @@ echo "Run 'docker push oommf/oommf:20a3' to push to dockerhub"
 # the 'latest' tag manually.
 #
 echo "Also run these commands to set the 'latest' tag to the new image:"
-echo "  docker tag oommf/oommf:20a3 oommf/oommf:latest"
+echo "  docker tag oommf/oommf:20b0 oommf/oommf:latest"
 echo "  docker push oommf/oommf:latest"
