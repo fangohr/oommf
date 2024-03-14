5c06dbcf051e8f338ca5ff4e575871a52beae944
Hans Fangohr <hans.fangohr@mpsd.mpg.de>
2024-03-14 15:57:30 +0100

    fixes check for buildx instance
    - tidy up
    - display commands before execution

--- docker/push-image-to-dockerhub.sh
+++ docker/push-image-to-dockerhub.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
 
 # login with docker hub account
 echo "Run 'docker login'"
@@ -10,10 +10,8 @@ echo "Build-multi-architecture container:"
 
 # if no builder exists yet, then create one. We use the exit code of `docker buildx inspect` to decide
 # (source: https://github.com/docker/buildx/discussions/1319)
-docker buildx inspect || echo docker buildx create --name container --driver=docker-container
+docker buildx inspect container || echo docker buildx create --name container --driver=docker-container
 
 # multi-platform build and push to Dockerhub
 time docker buildx build --tag oommf/oommf:latest tag --tag oommf/oommf:21a0 --platform linux/arm64,linux/amd64 --builder container --push .
-
-# TODO add  once working
 
