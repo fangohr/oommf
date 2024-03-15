04ea319a8950b0808ef9a732d2c6f163cc2cc903
Hans Fangohr <hans.fangohr@mpsd.mpg.de>
2024-03-15 17:42:12 +0100

    syntax error fix
    (remove superfluous "tag")

--- docker/push-image-to-dockerhub.sh
+++ docker/push-image-to-dockerhub.sh
@@ -10,8 +10,9 @@ echo "Build-multi-architecture container:"
 
 # if no builder exists yet, then create one. We use the exit code of `docker buildx inspect` to decide
 # (source: https://github.com/docker/buildx/discussions/1319)
-docker buildx inspect container || echo docker buildx create --name container --driver=docker-container
+docker buildx inspect container || docker buildx create --name container --driver=docker-container
 
 # multi-platform build and push to Dockerhub
-time docker buildx build --tag oommf/oommf:latest tag --tag oommf/oommf:21a0 --platform linux/arm64,linux/amd64 --builder container --push .
+time docker buildx build --tag oommf/oommf:latest --tag oommf/oommf:21a0 --platform linux/arm64,linux/amd64 --builder container --push .
+
 
