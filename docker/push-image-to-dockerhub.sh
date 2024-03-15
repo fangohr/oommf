3baad6a6d7c5ef57f4dc7e916b26b051603ebb9c
Hans Fangohr <hans.fangohr@mpsd.mpg.de>
2024-03-15 14:56:34 +0100

    fix: builder needs to be created if it does not exist yet
    There was an error before, so a missing builder (with name `container`) was not
    detected. Fixed here.

--- docker/push-image-to-dockerhub.sh
+++ docker/push-image-to-dockerhub.sh
@@ -10,7 +10,7 @@ echo "Build-multi-architecture container:"
 
 # if no builder exists yet, then create one. We use the exit code of `docker buildx inspect` to decide
 # (source: https://github.com/docker/buildx/discussions/1319)
-docker buildx inspect || echo docker buildx create --name container --driver=docker-container
+docker buildx inspect container || echo docker buildx create --name container --driver=docker-container
 
 # multi-platform build and push to Dockerhub
 time docker buildx build --tag oommf/oommf:latest tag --tag oommf/oommf:21a0 --platform linux/arm64,linux/amd64 --builder container --push .
